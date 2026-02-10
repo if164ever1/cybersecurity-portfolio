3db4ad4f9487b6163e6c4237b6eb8c59d6568eca
if164ever <zadorozhniak.ihor@gmail.com>
2026-02-10 16:45:02 -0600

    refactor test_connection.py to improve event reading logic and add event_types.py for event type mapping

---
+++ projects/Santinel Logs event AI Assistant/event_types.py
@@ -0,0 +1,3 @@
+# 1 - Error | 2 - Warning | 4 - Information | 8 - Audit Success | 16 - Audit Failure
+
+event_types_mapping = {"1": "Error", "2":"Warning", "4":"Information", "8":"Audit Success", "16":"Audit Failure"}
--- projects/Santinel Logs event AI Assistant/test_connection.py
+++ projects/Santinel Logs event AI Assistant/test_connection.py
@@ -1,34 +1,41 @@
+import time
 import win32evtlog as win
 import pywintypes
+import event_types
 
-server = 'localhost'
-logtype = 'System'
+SERVER = 'localhost'
+LOG_TYPE = 'System'
 
-def read_event(handle, flags): 
-    for event in win.ReadEventLog(handle, flags, 0):
-        event_id = event.EventID
-        time_generated = event.TimeGenerated
-        source_name = event.SourceName
-        # 1 - Error | 2 - Warning | 4 - Information | 8 - Audit Success | 16 - Audit Failure
-        event_type = event.EventType
-        print(f"Event ID: {event_id} - Time generated: {time_generated} - Source Name: {source_name}\n Event Type: {event_type}")
-
-try:
-    handle = win.OpenEventLog(server,logtype)
+def set_connection():
+    handle = win.OpenEventLog(SERVER,LOG_TYPE)
     flags = win.EVENTLOG_BACKWARDS_READ|win.EVENTLOG_SEQUENTIAL_READ
-    print(f"Successfully opened '{logtype}' event log on {server}.")
-    total_records = win.GetNumberOfEventLogRecords(handle)
-    print(f"Total number of records: {total_records}")
+    print(f"Successfully opened '{LOG_TYPE}' event log on {SERVER}.")
+    return handle, flags
     
-    read_event(handle, flags)
-    
-except pywintypes.error as e:
-    print(f"Win32 error opening event log: {e}")
-except Exception as e:
-    print(f"Unexpected error: {e}")
-finally:
+
+def read_event(handle, flags):
+    all_events = []
+    for event in win.ReadEventLog(handle, flags, 0):
+        extracted_data = {
+            "id": event_types.event_types_mapping.get(str(event.EventID), "Unknown"),
+            "time": event_types.event_types_mapping.get(str(event.TimeGenerated), "Unknown"),
+            "source": event_types.event_types_mapping.get(str(event.SourceName), "Unknown"),
+            "type": event_types.event_types_mapping.get(str(event.EventType), "Unknown")
+        }
+        all_events.append(extracted_data)
+    return all_events
+
+HADLE, FLAGS = set_connection()
+
+while 1:
     try:
-        if 'handle' in locals() and handle:
-            win.CloseEventLog(handle)
-    except Exception:
-        pass
+        time.sleep(2)
+        event_list = read_event(HADLE, FLAGS)
+        if event_list: 
+            print(event_list)
+    except pywintypes.error as e:
+        print(f"Win32 error opening event log: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+win.CloseEventLog(HADLE)
